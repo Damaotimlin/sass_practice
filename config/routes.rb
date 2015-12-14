8df88a26118bf3237dfecad7e8847de6321e0c11
Damaotimlin <damao.republic@gmail.com>
2015-12-14 15:14:18 +0000

    Added content

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :contacts do
-    
-    
-  end
-  
+  resources :contacts 
   get '/about' => 'pages#about'
   # You can have the root of your site routed with "root"
   root 'pages#home' # Keep root in the button
