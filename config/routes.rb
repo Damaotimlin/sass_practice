4b1ed699e485b4935c13f9144c4758792897b46e
Damaotimlin <damao.republic@gmail.com>
2015-12-14 16:38:41 +0000

    Added navbar route: contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :contacts 
+  resources :contacts # Auto generate many routes, Use 'bundle exec rake routes' to check the routes
   get '/about' => 'pages#about'
   # You can have the root of your site routed with "root"
   root 'pages#home' # Keep root in the button
