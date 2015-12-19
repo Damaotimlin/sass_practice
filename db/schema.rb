db1141f346d06a01dbb1da711785d0197a888c90
Damaotimlin <damao.republic@gmail.com>
2015-12-19 14:36:13 +0000

    Added association::Plan&users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219141731) do
+ActiveRecord::Schema.define(version: 20151219143106) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20151219141731) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "plan_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
