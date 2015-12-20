c3d304fc1eed7fb110ea5f03fbbab07b4dd4bd2b
Damaotimlin <damao.republic@gmail.com>
2015-12-20 08:47:44 +0000

    Modified devise for stripe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  # To make a whitelist pramaters for devise, to allow user send data to our server
+  before_filter :configure_permitted_paramaters, if: :devise_controller?
+  protected
+  def configure_permitted_paramaters
+    devise_paramater_sanitizer.for(:sign_up) { |user| 
+    user.permit(:name, :stripe_card_token, :email, :password, :password_configuration)
+    }
+  end
+  
 end
