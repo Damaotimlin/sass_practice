a85706238a6c2b2acc6d84760793929ffdc7c9e7
Damaotimlin <damao.republic@gmail.com>
2015-12-20 08:15:33 +0000

    Added a new column into users to saves stripe_customer_token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219143106) do
+ActiveRecord::Schema.define(version: 20151220081053) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20151219143106) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "plan_id"
+    t.string   "stripe_customer_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
