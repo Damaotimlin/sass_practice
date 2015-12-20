4ec6375fc0694b26f00ecc4d52ad1ec8fd963ab4
Damaotimlin <damao.republic@gmail.com>
2015-12-20 08:38:37 +0000

    Added stripe_customer_token column to users table

--- db/migrate/20151219143106_add_plan_to_user.rb
+++ db/migrate/20151219143106_add_plan_to_user.rb
@@ -1,5 +1,6 @@
 class AddPlanToUser < ActiveRecord::Migration
   def change
+    # SQL-command :which table, :what you want to added into, :data type
     add_column :users, :plan_id, :integer
   end
 end
--- db/migrate/20151220081053_add_stripe_customer_token_to_user.rb
+++ db/migrate/20151220081053_add_stripe_customer_token_to_user.rb
@@ -1,5 +1,6 @@
 class AddStripeCustomerTokenToUser < ActiveRecord::Migration
   def change
+    # SQL-command :which table, :what you want to added into, :data type
     add_column :users, :stripe_customer_token, :string
   end
 end
