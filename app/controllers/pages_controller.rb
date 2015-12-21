fd16f7e143cf67c897ddb35f02aa2736ef81faa3
Damaotimlin <damao.republic@gmail.com>
2015-12-21 16:38:30 +0000

    Added some feature

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,31 @@ class PagesController < ApplicationController
     def home
         @basic_plan = Plan.find(1)
         @pro_plan = Plan.find(2)
-        
         if user_signed_in?
             @user = User.find(current_user)
+        end
+        
+        if current_user
+            @rand_user_id = check_profile_id(rand(1..User.all.length))
         end
     end
     
     def about
         
     end
+    
+    private
+    def check_profile_id(n)
+        u = User.find(n)
+        if u.profile
+            if u.profile.first_name != nil && u.profile.first_name != '' && u != User.find(current_user)
+                @rand_user = User.find(u)
+            else
+                 @rand_user_id = check_profile_id(rand(1..User.all.length))
+            end
+        else 
+             @rand_user_id = check_profile_id(rand(1..User.all.length))
+        end
+    end
+    
 end
