77f5597abe3f0e926a390168a17b8515ada8f728
Damaotimlin <damao.republic@gmail.com>
2015-12-21 03:58:38 +0000

    Added some comment

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,11 +4,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
     before_filter :select_plan, only: :new # Declare the name :select_plan and only been used while "new" Devises' controller
     
     def create
-        super do |resource|
+        super do |resource| # resource is the data from HTML, in this case is User registration info
             if params[:plan] # if do have :plan params have been submit with form
                 resource.plan_id = params[:plan] # Save the :plan id to resource.plan_id value; plan_id is Users DB table columns; In this case will be 1 or 2
                 if resource.plan_id == 2
-                    resource.save_with_payment # If this resource comes from plan_id=2, saves by special way
+                    resource.save_with_payment # If this resource comes from plan_id=2, saves by special way. The custom function is define at user.rb Model
                 else
                     resource.save # If plan_id=1, just saves as defult
                 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,10 @@ class User < ActiveRecord::Base
     if valid? # We did added some validation to make sure user filled in properly ::contact.rb
       # After Stripe get this info, Stripe will do the charging, this method comes from Stripe gem
       # Stripe will return an id after charging
-      customer = Stripe::Customer.create(description: email, plan: plan_id, source: stripe_card_token) # Stripe had UG their API from card: -> source:
-      # Set a property to the user after received the id Stripe returned
-      self.stripe_customer_token = customer.id
+      customer = Stripe::Customer.create(description: email, plan: plan_id, source: stripe_card_token) # Stripe had upgrade their API from card: -> source:
+      # Set a property to the user after received the id Stripe returned(stripe will return customer hash)
+      # 'self' means the target of this function, in this case is User, and we set a new atribute stripe_customer_token to User. 
+      self.stripe_customer_token = customer.id # Setting stripe_customer_token to Stripe::customer.id 
       save!
     end
     
